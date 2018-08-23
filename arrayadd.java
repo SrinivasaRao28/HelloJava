75c11f133693c2df04c3bfd39614d5c3ac1e53c5
srinivas <srinivas074a3@gmail.com>
2018-08-23 18:24:17 +0530

    second commit

--- arrayadd.java
+++ arrayadd.java
@@ -1,7 +1,7 @@
 class Addition{
 public static void main(String []args){
-int[][] a={{1,1,1,}};
-int[][] b={{1,1,1},{2,2,2}};
+int[][] a={{1,1,1,},{1,1,1},{1,1,1}};
+int[][] b={{1,1,1},{2,2,2},{1,1,1}};
 int[][] c=new int[3][3];
 
 for(int i=0;i<a.length;i++){
