e64f0f3f232251880489a21d886697a3eaa11ced
srinivas <srinivas074a3@gmail.com>
2018-09-12 17:56:53 +0530

    arrayadd.java file modified

--- arrayadd.java
+++ arrayadd.java
@@ -18,6 +18,20 @@ c[i][j] +=a[i][k]*b[k][j];
 
 System.out.print(c[i][j]+"  ");
 }
+for(int i=0;i<a.length;i++){
+	
+	
+for(int j=0;j<b[0].length;j++){
+	c[i][j]=0;
+	for(int k=0;k<3;k++){
+		
+	
+c[i][j] +=a[i][k]*b[k][j];
+
+	}
+
+System.out.print(c[i][j]+"  ");
+}
 System.out.println();
 }
 }
